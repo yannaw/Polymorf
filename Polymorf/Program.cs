5b87ecb6294955bbcbc2b2b314a37461124106db
yannaw <yannaw@hotmail.com>
2017-05-30 15:39:18 +0200

    Add project files.

---
+++ Polymorf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorf {
+    class Program {
+        static void Main(string[] args) {
+            List<UserError> errors = new List<UserError>();
+            for(int i = 0; i < 3; i++) {
+                errors.Add(new NumericInputError());
+                errors.Add(new TextInputError());
+                errors.Add(new ProgramInputError());
+                errors.Add(new CatInputError());
+                errors.Add(new SleepInputError());
+            }
+
+            int c = 0;
+            foreach(var e in errors) {
+                Console.WriteLine("{1}", c, e.UEMessage());
+                c++;
+            }
+
+
+        }
+    }
+}
